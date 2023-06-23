da4fbfb9c58fec50e4830b2494cb3d01dbb2e6e0
Lukas Halbritter <halbi93@gmx.de>
2023-06-23 14:16:49 +0200

    Completed chapter 6.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,6 +1,5 @@
 from odoo import fields, models
 
-
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "An estate property."
@@ -8,13 +7,28 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False,
+        default=lambda self: fields.Date.add(
+            fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection([("N", "North"), ("S", "South"), ("E", "East"), ("W", "West")])
+    active = fields.Boolean(default=False)
+    state = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Canceled")
+        ],
+        required=True,
+        copy=False,
+        default="new")
