3fdbf51c1c7e9eb00a10b4f8db02b31a6b4595af
Lukas Halbritter <halbi93@gmx.de>
2023-06-28 16:01:39 +0200

    Tinkering around with testing views.

--- estate/tests/test_estate_property_type.py
+++ estate/tests/test_estate_property_type.py
@@ -6,10 +6,16 @@ from odoo import fields
 from odoo.tools import mute_logger
 from odoo.tests.common import TransactionCase
 from psycopg2.errors import NotNullViolation
+from lxml import html
+from typing import Dict
 
 class EstatePropertyTypeTests(TransactionCase):
     def setUp(self):
         self._model = self.env['estate.property.type']
+        self._model_mock = self._model.create(
+            {
+                'name': 'House'
+            })
 
     def test_create_new_estate_property_type(self):
         record = self._model.create({"name": "House"})
@@ -19,3 +25,23 @@ class EstatePropertyTypeTests(TransactionCase):
     def test_estate_property_type_name_is_required(self):
         call = lambda: self._model.create({"name": None})
         self.assertRaises(NotNullViolation, call)
+
+    def test_estate_property_type_tree_has_desired_columns(self):
+        tree_view = self.get_tree_view_for_model('estate.property.type')
+        architecture = tree_view["arch"]
+        architecture_xpath = html.fromstring(architecture)
+
+        column_names = [
+            field.get("name")
+            for field in architecture_xpath.xpath('//tree/field')]
+
+        expected_column_names = ['name']
+
+        self.assertListEqual(column_names, expected_column_names)
+
+    def get_tree_view_for_model(self, model_name: str) -> Dict:
+        return self._get_view_for_model(model_name, 'tree')
+
+    def _get_view_for_model(self, model_name: str, view_type: str) -> Dict:
+         model = self.env[model_name]
+         return model.get_view(view_type=view_type)
