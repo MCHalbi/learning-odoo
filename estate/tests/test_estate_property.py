f8471d1b3f8d4c563b751bb3ced7dfded099035c
Lukas Halbritter <halbi93@gmx.de>
2023-06-28 16:59:44 +0200

    Added test for model form view.

--- estate/tests/test_estate_property.py
+++ estate/tests/test_estate_property.py
@@ -17,6 +17,15 @@ class EstatePropertyTests(TransactionCase):
         self.assertModelHasAttributeWithType("postcode", fields.Char)
         self.assertModelHasAttributeWithType("date_availability", fields.Date)
 
+    def test_estate_property_uses_correct_form_view(self):
+        # Check, that not the default view is used
+        actual_view_id = self._model.get_view(view_type='form')['id']
+        self.assertTrue(actual_view_id)
+
+        # Check, that the XML id of the used view is correct
+        actual_view_xml_id = self.env['ir.ui.view'].browse(actual_view_id).xml_id
+        self.assertEqual(actual_view_xml_id, 'estate.estate_property_view_form')
+
     def assertModelHasAttributeWithType(
             self,
             attributeName: str,
