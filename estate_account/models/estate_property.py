f558b5c1b46abc1e41b1e58c3fdf145c02ed2f2e
Lukas Halbritter <halbi93@gmx.de>
2023-07-24 13:57:43 +0200

    Add invoice creation logic for 'sold' button
    When clicking the "SOLD" button in the estate property view, an empty
    invoice is created for the buyer.

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,7 +1,23 @@
 from odoo import fields, models
 
+
 class EstateProperty(models.Model):
     _inherit = "estate.property"
 
     def sell(self):
-        return super().sell()
+        result = super().sell()
+        journal = self.env["account.journal"].search(
+            [("type", "=", "sale")], limit=1
+        )
+
+        for estate_property in self:
+            invoice_data = {
+                "partner_id": estate_property.buyer_id.id,
+                "move_type": "out_invoice",
+                "journal_id": journal.id,
+            }
+            print(invoice_data)
+
+            self.env["account.move"].create(invoice_data)
+
+        return result
