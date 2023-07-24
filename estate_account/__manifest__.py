d38b2457331c7058480d1d281153e93227b55312
Lukas Halbritter <halbi93@gmx.de>
2023-07-24 10:45:37 +0200

    Create empty estate_account module

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "estate_account",
+    'depends': [
+        'base',
+        'estate',
+        'account',
+    ],
+
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+}
