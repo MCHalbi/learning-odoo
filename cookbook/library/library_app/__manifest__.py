cc9b857a4965686b0d0300653c241ea39b5819bb
Lukas Halbritter <halbi93@gmx.de>
2023-09-25 11:43:06 +0200

    Add top menu item

--- cookbook/library/library_app/__manifest__.py
+++ cookbook/library/library_app/__manifest__.py
@@ -3,7 +3,12 @@
     "name": "Library Management",
     "summary": """Manage library catalog and book lengding.""",
     "author": "Lukas Halbritter",
+
     "category": "Services/Library",
+
     "depends": ["base"],
+    "data": [
+        "views/library_menu.xml",
+    ],
     "application": True,
 }
