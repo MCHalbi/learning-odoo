32c1cb999443a18ddb0136f796a02690de9781a8
Lukas Halbritter <halbi93@gmx.de>
2023-09-25 13:35:28 +0200

    Add library manager and user group

--- cookbook/library/library_app/__manifest__.py
+++ cookbook/library/library_app/__manifest__.py
@@ -3,11 +3,10 @@
     "name": "Library Management",
     "summary": """Manage library catalog and book lengding.""",
     "author": "Lukas Halbritter",
-
     "category": "Services/Library",
-
     "depends": ["base"],
     "data": [
+        "security/library_security.xml",
         "views/library_menu.xml",
     ],
     "application": True,
