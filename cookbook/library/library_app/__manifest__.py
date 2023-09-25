ef8fb21ab54641a3eff81e1f88c14f255e1bf87b
Lukas Halbritter <halbi93@gmx.de>
2023-09-25 10:23:21 +0200

    Add module to Services/Library category

--- cookbook/library/library_app/__manifest__.py
+++ cookbook/library/library_app/__manifest__.py
@@ -3,6 +3,7 @@
     "name": "library_app",
     "summary": """Manage library catalog and book lengding.""",
     "author": "Lukas Halbritter",
+    "category": "Services/Library"
     "depends": ["base"],
     "application": True,
 }
